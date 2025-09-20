be1e2c035a18bab85e457496c0ea860aa628cafb
shad0w <sacredwolfshadow@gmail.com>
2025-09-20 19:10:21 +0000

    gg

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 import setuptools
 
+def find_packages():
+    return setuptools.find_packages(exclude=("test*",))
+
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
 setuptools.setup(
     name="OctoPrint-HandCode",
     version="1.0.0",
-    description="An OctoPrint plugin to generate handwriting GCode using HandCode.",
     author="Your Name",
     author_email="your.email@example.com",
-    packages=setuptools.find_packages(),
-    install_requires=["requests"],
+    url="https://github.com/YourUsername/OctoPrint-HandCode",
+    description="Integrates the HandCode handwriting GCode generator with OctoPrint.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="AGPLv3",
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    include_package_data=True,
+    zip_safe=False,
     entry_points={
         "octoprint.plugin": [
             "handcode = octoprint_handcode"
         ]
-    }
+    },
+    python_requires=">=3.7",
 )
+
